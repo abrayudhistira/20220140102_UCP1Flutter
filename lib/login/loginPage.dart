10884b64e26261e31fa4e709c54f85323b4a58ed
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2025-04-25 09:52:02 +0700

    declaration variable of _usernameController and _passwordController

--- lib/login/loginPage.dart
+++ lib/login/loginPage.dart
@@ -8,6 +8,10 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
