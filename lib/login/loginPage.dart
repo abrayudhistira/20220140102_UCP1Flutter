562af11dd103dc094d8ca06e4763758fb2f7ccb9
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2025-04-25 10:20:33 +0700

    Create Email Form Field

--- lib/login/loginPage.dart
+++ lib/login/loginPage.dart
@@ -9,7 +9,7 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   
-  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
   
   @override
@@ -28,6 +28,21 @@ class _LoginPageState extends State<LoginPage> {
               Text(
                 "Selamat Datang Kembali",
                 style: Theme.of(context).textTheme.headlineLarge,
+              ),
+              TextFormField(
+                controller: _emailController,
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  prefixIcon: new Icon(Icons.email),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Harap Masukkan Email';
+                  }
+                },
               ),
             ],
           ),
