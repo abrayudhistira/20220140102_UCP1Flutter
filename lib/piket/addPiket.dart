e6b9cbe564d6ce03467c783f0ad40ff1954482a8
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2025-04-28 01:15:57 +0700

    Create Piket Page

---
+++ lib/piket/addPiket.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AddPiket extends StatefulWidget {
+  const AddPiket({ Key? key }) : super(key: key);
+
+  @override
+  _AddPiketState createState() => _AddPiketState();
+}
+
+class _AddPiketState extends State<AddPiket> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
