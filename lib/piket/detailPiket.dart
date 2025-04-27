0f8300aeec27ba60ee167ef5b843d0746476e407
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2025-04-28 03:43:34 +0700

    Fixed UI Detail Piket

--- lib/piket/detailPiket.dart
+++ lib/piket/detailPiket.dart
@@ -33,14 +33,58 @@ class DetailPiket extends StatelessWidget {
             child: Center(
               child: Column(
                 children: [
-                  Text(
-                    '$email',
+                  Row(
+                    children: [
+                      Text(
+                        formatDate(date),
+                        style: const TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.deepOrange,
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 30,
+                      ),
+                      Card(
+                        color: Colors.deepOrange,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 12.0,
+                            vertical: 8.0,
+                          ),
+                          child: Text(
+                            email,
+                            style: const TextStyle(
+                              fontSize: 14,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                  Text(
-                    '${formatDate(date)}',
+                  const SizedBox(
+                    height: 20,
                   ),
-                  Text(
-                    '$task',
+                  Card(
+                    color: Colors.deepOrange,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Text(
+                        task,
+                        style: const TextStyle(
+                          fontSize: 16,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
                   ),
                 ],
               ),
